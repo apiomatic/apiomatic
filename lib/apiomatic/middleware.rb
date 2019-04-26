baad68300142faa22524e76363ee2f5d9b6bc261
Andrew Maraev <the_vk@thevk.net>
2019-04-25 22:06:30 -0700

    Basic interface to configure routing table

--- lib/apiomatic/middleware.rb
+++ lib/apiomatic/middleware.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
+require 'apiomatic/http_verbs'
+require 'apiomatic/middleware_config'
+
 module Apiomatic
+  class << self
+    def middleware
+      config = MiddlewareConfig.new
+      yield config
+      Middleware.new(config)
+    end
+  end
+
   class Middleware
+    def initialize(config)
+      @config = config
+    end
+
     def call(env)
-      [204, {}, []]
+      [204, {}, [env.inspect]]
     end
   end
 end
---
+++ lib/apiomatic/middleware_config.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Apiomatic
+  class MiddlewareConfig
+    def map(routing_table) ; end
+  end
+end
